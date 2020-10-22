a55107ad98ea82684439898fb3ea7b5fee489b20
saradhivs <saradhivs@yahoo.com>
2020-10-22 16:07:06 -0400

    Made changes for CORS errors.

--- src/main/java/org/piangles/gateway/service/impl/jetty/GatewayServiceImpl.java
+++ src/main/java/org/piangles/gateway/service/impl/jetty/GatewayServiceImpl.java
@@ -1,16 +1,28 @@
 package org.piangles.gateway.service.impl.jetty;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import javax.servlet.DispatcherType;
+
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
-import org.piangles.gateway.Constants;
-import org.piangles.gateway.GatewayService;
-
 import org.piangles.core.services.remoting.SessionAwareable;
 import org.piangles.core.services.remoting.SessionDetails;
 import org.piangles.core.services.remoting.SessionDetailsCreator;
+import org.piangles.gateway.Constants;
+import org.piangles.gateway.GatewayService;
 
 public class GatewayServiceImpl implements GatewayService
 {
@@ -37,7 +49,29 @@ public class GatewayServiceImpl implements GatewayService
 				factory.register(WebSocketLifecycleEventHandler.class);
 			}
 		};
+		
+        ServletContextHandler defaultContext = new ServletContextHandler();
+        FilterHolder cors = defaultContext.addFilter(CrossOriginFilter.class,"/*",EnumSet.of(DispatcherType.REQUEST));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
 
+        // Use a DefaultServlet to serve static files.
+        // Alternate Holder technique, prepare then add.
+        // DefaultServlet should be named 'default'
+        ServletHolder defaultServletHolder = new ServletHolder("default", DefaultServlet.class);
+        defaultServletHolder.setInitParameter("resourceBase","./http/");
+        defaultServletHolder.setInitParameter("dirAllowed","false");
+        defaultContext.addServlet(defaultServletHolder,"/");
+        
+        List<Handler> webSocketHandlerList = new ArrayList<>();
+        webSocketHandlerList.add(defaultContext);
+        
+        HandlerCollection handlerCollection = new HandlerCollection();
+        handlerCollection.setHandlers(webSocketHandlerList.toArray(new Handler[0]));
+        server.setHandler(handlerCollection);
+        
 		server.setHandler(wsHandler);
 		server.start();
 		server.dump(System.err);
@@ -51,7 +85,7 @@ public class GatewayServiceImpl implements GatewayService
 	class GatewayThreadPool extends QueuedThreadPool
 	{
 		@Override
-		protected Thread newThread(Runnable runnable)
+		public Thread newThread(Runnable runnable)
 		{
 			return new ClientHandlerThread(runnable);
 		}
