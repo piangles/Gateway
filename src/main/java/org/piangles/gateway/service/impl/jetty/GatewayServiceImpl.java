a9ace053082b4d92e992f570bab042ab9f8788f2
saradhivs <saradhivs@yahoo.com>
2020-12-16 09:18:58 -0500

    Incorporated web app to expose EndpointMetadata.

--- src/main/java/org/piangles/gateway/service/impl/jetty/GatewayServiceImpl.java
+++ src/main/java/org/piangles/gateway/service/impl/jetty/GatewayServiceImpl.java
@@ -1,23 +1,21 @@
 package org.piangles.gateway.service.impl.jetty;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.List;
 
 import javax.servlet.DispatcherType;
 
-import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.http.pathmap.ServletPathSpec;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.eclipse.jetty.websocket.server.WebSocketHandler;
-import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.piangles.core.services.remoting.SessionAwareable;
 import org.piangles.core.services.remoting.SessionDetails;
 import org.piangles.core.services.remoting.SessionDetailsCreator;
@@ -30,6 +28,13 @@ public class GatewayServiceImpl implements GatewayService
 
 	public GatewayServiceImpl()
 	{
+		/**
+		 * Jetty HTTP Servlet Server.
+		 * This class is the main class for the Jetty HTTP Servlet server.
+		 * It aggregates Connectors (HTTP request receivers) and request Handlers.
+		 * The server is itself a handler and a ThreadPool.  Connectors use the ThreadPool methods
+		 * to run jobs that will eventually call the handle method.
+		 */
 		server = new Server(new GatewayThreadPool());
 	}
 
@@ -38,42 +43,58 @@ public class GatewayServiceImpl implements GatewayService
 		ServerConnector connector = new ServerConnector(server);
 		connector.setHost(host);
 		connector.setPort(port);
+		
+		//A server can listen on multiple ports ex: http and https
 		server.addConnector(connector);
 
-		// Setup the basic application "context" for this application at "/"
-		// This is also known as the handler tree (in jetty speak)
-		WebSocketHandler wsHandler = new WebSocketHandler()
-		{
-			@Override
-			public void configure(WebSocketServletFactory factory)
-			{
-				factory.register(WebSocketLifecycleEventHandler.class);
-			}
-		};
-		
+		/**
+		 * Setup the basic application "context" for this application at "/"
+		 * This is also known as the handler tree (in jetty speak)
+
+         * Use a DefaultServlet to serve static files. Alternate Holder technique, prepare then add.
+         * DefaultServlet should be named 'default'
+         */
         ServletContextHandler defaultContext = new ServletContextHandler();
+        defaultContext.setContextPath("/");
+        defaultContext.setWelcomeFiles(new String[] { "index.html" });
+        /**
+         * web is the folder under resources where index.html exists
+         * the index.html needs to reside under a subdirectory of resources.
+         */
+        String path = getClass().getClassLoader().getResource("web").toExternalForm();
+        defaultContext.setResourceBase(path);
+        defaultContext.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed","false");
+        defaultContext.addServlet(DefaultServlet.class, "/");
+        
+        /**
+         * This is to allow for CORS, when the UI is served from one host but the
+         * Webservices (Websocket API) is on another server.
+         */
         FilterHolder cors = defaultContext.addFilter(CrossOriginFilter.class,"/*",EnumSet.of(DispatcherType.REQUEST));
         cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
         cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
         cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
         cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
 
-        // Use a DefaultServlet to serve static files.
-        // Alternate Holder technique, prepare then add.
-        // DefaultServlet should be named 'default'
-        ServletHolder defaultServletHolder = new ServletHolder("default", DefaultServlet.class);
-        defaultServletHolder.setInitParameter("resourceBase","./http/");
-        defaultServletHolder.setInitParameter("dirAllowed","false");
-        defaultContext.addServlet(defaultServletHolder,"/");
+		/**
+		 * Create a Handler for Websocket protocol, on connection from a client
+		 * the server determines the protocol from url
+		 * http://host:port/
+		 * or
+		 * ws://host:port 
+		 * and uses the appropriate handler. So it it sends an Upgrade message.
+		 * 
+		 * TODO
+		 * Configure websocket behavior
+		 * wsfilter.getFactory().getPolicy().setIdleTimeout(5000);
+		 */
+        // Add the websocket filter
+        WebSocketUpgradeFilter wsfilter = WebSocketUpgradeFilter.configure(defaultContext);
+        // Add websocket mapping
+        wsfilter.addMapping(new ServletPathSpec("/api/"),new WebSocketCreatorImpl());
         
-        List<Handler> webSocketHandlerList = new ArrayList<>();
-        webSocketHandlerList.add(defaultContext);
-        
-        HandlerCollection handlerCollection = new HandlerCollection();
-        handlerCollection.setHandlers(webSocketHandlerList.toArray(new Handler[0]));
-        server.setHandler(handlerCollection);
-        
-		server.setHandler(wsHandler);
+		server.setHandler(defaultContext);
+		
 		server.start();
 		server.dump(System.err);
 	}
@@ -83,6 +104,13 @@ public class GatewayServiceImpl implements GatewayService
 		server.join();
 	}
 
+	/**
+	 * A thread pool for the Server to create ClientHandlerThread.
+	 * Everytime a client establishes connection a ClientHandlerThread
+	 * from the pool services the connection.
+	 * 
+	 * This is protocol agnostic.
+ 	 */
 	class GatewayThreadPool extends QueuedThreadPool
 	{
 		@Override
@@ -92,6 +120,19 @@ public class GatewayServiceImpl implements GatewayService
 		}
 	}
 
+    class WebSocketCreatorImpl implements WebSocketCreator
+    {
+        @Override
+        public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp)
+        {
+            return new WebSocketLifecycleEventHandler();
+        }
+    }
+
+	/**
+	 * This needs to be SessionAwareable to obtain access to basic services
+	 * like logging.
+	 */
 	class ClientHandlerThread extends Thread implements SessionAwareable
 	{
 		private Runnable runnable = null;
